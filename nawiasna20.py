1affc04c53a94b99caf4645459fdd415f48187ec
AndrekMYT <82237604+AndrekMYT@users.noreply.github.com>
2022-03-28 17:35:27 +0200

    first commit

---
+++ nawiasna20.py
@@ -0,0 +1,14 @@
+import sys
+
+
+n = int(sys.stdin.readline())
+
+if n %2 == 0:
+    x = '('*n
+    y = ')' *n
+    print(f'{x}{y}')
+else:
+    nmod = n//2
+    x = '('*nmod
+    y = ')' *nmod
+    print(f'{x}{y}{x}{y}')
